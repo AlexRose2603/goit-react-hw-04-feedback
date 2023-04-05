432e0dfa2b370c4dbda2146df67bcbe44c932bca
AlexRose2603 <alexandra26korchak@gmail.com>
2023-04-05 11:43:33 +0300

    init

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  border-radius: 72px;
+  background: #29426a;
+  box-shadow: 32px 32px 64px #121c2e, -32px -32px 64px #4068a6;
+  margin: 30px auto 0;
+  width: 600px;
+  padding: 10px;
+`;
